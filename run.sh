aefcf8491325ddf89a37eec6135aaa42e861fb56
mrzhang <2529450174@qq.com>
2015-11-26 08:45:58 +0800

    in work-place

--- run.sh
+++ run.sh
@@ -10,10 +10,11 @@ if [ ! -f $base_dir/env/bin/activate ]
 then
 	# cd $project_env
 	virtualenv $base_dir/env
+	sudo $base_dir/env/bin/pip install -r $base_dir/requirements.txt
 	
 fi
 
 
 source  $base_dir/env/bin/activate
-sudo $base_dir/env/bin/pip install -r $base_dir/requirements.txt
+
 python $base_dir/run.py
